4c57415228fa5be9ba4a388f45e513afa23d7f5b
pinkyrosewilson11 <pinkyrosewilson11@gmail.com>
2021-03-23 13:46:49 +1100

    Added the Project YourStore

---
+++ YourStore/src/main/java/YourStore/YourStore/App.java
@@ -0,0 +1,13 @@
+package YourStore.YourStore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
