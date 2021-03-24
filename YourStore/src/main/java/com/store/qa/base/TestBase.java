df1daecb2aac0d09907a9febb9cbeae4833886a1
pinkyrosewilson11 <pinkyrosewilson11@gmail.com>
2021-03-24 11:01:29 +1100

    Updated Testbase class and Register base class

--- YourStore/src/main/java/com/store/qa/base/TestBase.java
+++ YourStore/src/main/java/com/store/qa/base/TestBase.java
@@ -1,5 +1,19 @@
 package com.store.qa.base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TestBase {
+
+	WebDriver w= new ChromeDriver();
+	
+	public void main() throws InterruptedException
+	{
+		w.get("http://tutorialsninja.com/demo/index.php?route=common/home");
+		w.manage().window().maximize();
+		w.manage().deleteAllCookies();
+	    Thread.sleep(2000);
+		
+	}
 
 }
--- YourStore/src/main/java/com/store/qa/pages/RegisterPage.java
+++ YourStore/src/main/java/com/store/qa/pages/RegisterPage.java
@@ -1,5 +1,14 @@
 package com.store.qa.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class RegisterPage {
+	
+	@FindBy(xpath="//span[contains(text(),'My Account')]") WebElement myaccount;
+	@FindBy(xpath="//a[contains(text(),'Register')]") WebElement register;
+	
+	
+	
 
 }
