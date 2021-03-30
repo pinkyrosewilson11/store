f3ec50440a8b4882587926a83f6a8b3228256594
pinkyrosewilson11 <pinkyrosewilson11@gmail.com>
2021-03-30 16:25:28 +1100

    Login page

--- YourStore/src/main/java/com/store/qa/pages/LoginPage.java
+++ YourStore/src/main/java/com/store/qa/pages/LoginPage.java
@@ -1,5 +1,13 @@
 package com.store.qa.pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class LoginPage {
+	
+	public  LoginPage(WebDriver driver)
+	{
+		
+	}
+	
 
 }
--- YourStore/src/test/java/com/store/qa/testcases/LoginTest.java
+++ YourStore/src/test/java/com/store/qa/testcases/LoginTest.java
@@ -1,14 +1,35 @@
 package com.store.qa.testcases;
 
 import org.testng.annotations.Test;
+
+import com.store.qa.pages.LoginPage;
+import com.store.qa.util.utilpage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
 public class LoginTest {
-  @Test
-  public void f() {
-  }
-  @BeforeMethod
-  public void beforeMethod() {
-  }
+	
+	WebDriver driver;
+	LoginPage login;
+	utilpage util;
 
+  @BeforeTest
+  public void setup() {
+	  
+	  driver=new ChromeDriver();
+	  driver.get("http://tutorialsninja.com/demo/");
+	  driver.manage().window().maximize();
+	  driver.manage().deleteAllCookies();
+	  login= new LoginPage(driver);
+	  util=new utilpage();
+	  
+	  
+	  
+  }
+  
+  
+  
 }
